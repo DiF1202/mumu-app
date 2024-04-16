1c8ffac8e9ee4cc53063183c46a5856b6f92e11d
http://192.168.1.180:8088/u/jkc <794334242@qq.com>
2024-04-16 23:18:59 +0800

    feat: 登录页UI

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,36 +1,31 @@
 <template>
-  <view class="login-container">
-    <up-button type="primary" text="确定"></up-button>
-    <view class="text">scss</view>
-    <up-cell icon="share" title="停车坐爱枫林晚" :isLink="true" arrow-direction="down"></up-cell>
-    <up-tabs :list="list"></up-tabs>
+  <view class="login-container" :style="{paddingTop:`${safetyTop}px`,height: `${windowHeight}px` }">
+
   </view>
 </template>
 
 <script>
   export default {
     data() {
-      return {
-        list: [
-          { name: '关注' },  
-          { name: '推荐' },  
-          { name: '电影' },  
-          { name: '科技' },  
-          { name: '音乐' },  
-          { name: '美食' },  
-          { name: '文化' },  
-          { name: '财经' },  
-          { name: '手工' } 
-        ]
+      return {}
+    },
+    computed: {
+      safetyTop() {
+        return uni.getSystemInfoSync().safeAreaInsets.top
+      },
+      windowHeight() {
+        return uni.getSystemInfoSync().windowHeight - 50
       }
+    },
+    onLoad() {
+    },
+    methods: {
     }
   }
 </script>
 
 <style lang="scss" scoped>
   .login-container {
-    .text {
-      color: blue;
-    }
+    background-image: linear-gradient(to bottom, #10cc8f 0%, #ffffff 50%)
   }
 </style>
