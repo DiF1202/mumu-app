8cd1a0c139d587ff67d4e2838f626902666684bb
http://192.168.1.180:8088/u/jkc <794334242@qq.com>
2024-04-18 23:33:31 +0800

    feat: 列表组件添加

--- src/pages/view/index.vue
+++ src/pages/view/index.vue
@@ -1,48 +1,25 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+    <u-button type="primary" :plain="true" text="列表页" @click="listPage"></u-button>
+		<u-button type="primary" :plain="true" :hairline="true" text="详情页"></u-button>
   </view>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      title: "远程查看"
-    };
+    return {}
   },
   onLoad() {},
-  methods: {}
+  methods: {
+    listPage() {
+      uni.navigateTo({
+         url: '/pages/view/components/list'
+      })
+    }
+  }
 };
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
 </style>
