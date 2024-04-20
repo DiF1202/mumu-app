a0db7a50a32b82b289b9b17625c198c31b28ac83
http://192.168.1.180:8088/u/jkc <794334242@qq.com>
2024-04-20 15:05:12 +0800

    feat: 创建环境详情文件

---
+++ src/pages/home/environment/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="env-container">
+    <uni-navtopbbar title="环境详情" :back="true"></uni-navtopbbar>
+    <u-gap :height="`${safetyTop + 40}px`" bgColor="#D6E7FF"></u-gap>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		computed: {
+      safetyTop() {
+        return uni.getSystemInfoSync().safeAreaInsets.top
+      }
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.env-container {
+    .content {
+      background: linear-gradient(to bottom, #D6E7FF 0%, #FFFFFF 600rpx);
+      padding: 0 24rpx 24rpx;
+      .env-warning {
+        width: 100%;
+        .env-row {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          margin: 24rpx 0;
+        }
+      }
+      .assets-charts {
+        width: 100%;
+        height: 200rpx;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 24rpx;
+        .chart-item {
+          width: 200rpx;
+          height: 200rpx;
+        }
+      }
+      .daily-briefing {
+        width: 100%;
+        height: 500rpx;
+      }
+    }
+	}
+</style>
