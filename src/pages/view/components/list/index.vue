407a9f19ca821be1c4734076e51572ab46de7c1f
http://192.168.1.180:8088/u/jkc <794334242@qq.com>
2024-04-28 23:25:07 +0800

    feat: 完善实况视频UI

--- src/pages/view/components/list/index.vue
+++ src/pages/view/components/list/index.vue
@@ -1,60 +1,47 @@
 <template>
   <view class="list-container">
-    <view> </view>
-
-    <view class="section">
-      <view class="section-title">实时监控</view>
-      <view class="section-video">
+    <uni-navtopbar title="实况视频" :back="true"></uni-navtopbar>
+    <view class="content">
+      <uni-treeSelect :columns="columns" @treeCallback="treeCallback"/>
+      <view class="video-section">
         <view id="mui-player"></view>
       </view>
-    </view>
-
-    <view class="section">
-      <view class="section-title">告警事件</view>
-      <u-list
-        @scrolltolower="loadmore"
-        lowerThreshold="100"
-        :height="windowHeight - listHeight"
-      >
-        <u-list-item
-          class="list"
-          v-for="(item, index) in listData"
-          :key="index"
+      <view class="warin-section">
+        <u-list
+          @scrolltolower="loadmore"
+          lowerThreshold="100"
+          :height="windowHeight - 382"
         >
-          <view class="list-item" @click="enterDetails">
-            <view class="item-header">
-              <u--text
-                suffixIcon="arrow-right"
-                iconStyle="font-size: 18px; color: #333333; font-weight:bold"
-                :text="item.title"
-                size="36rpx"
-                color="#333333"
-                :bold="true"
-              ></u--text>
-              <view class="status">
-                <view class="status-item">未处理</view>
+          <u-list-item
+            v-for="(item, index) in listData"
+            :key="index"
+          >
+            <view class="list-item" @click="enterDetails">
+              <u--image :showLoading="true" src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg" width="320rpx" height="210rpx"></u--image>
+              <view class="item-info">
+                <view class="item-header"></view>
+                <u--text :text="item.title" size="36rpx" color="#333333" :bold="true"></u--text>
+                <u--text :text="'时间：' + item.time" size="28rpx" color="#333333"></u--text>
+                <view class="status-tag">
+                  <u-tag :text="item.status === '1' ? '已处理' : '未处理'" :type="item.status === '1' ? 'success' : 'error'" shape="circle" size="mini"></u-tag>
+                </view>
+                <view class="select-item">
+                  <u-checkbox-group v-model="item.select" >
+                    <u-checkbox :name="item.id" label=""></u-checkbox>
+                  </u-checkbox-group>
+                </view>
               </view>
             </view>
-            <view class="item-content">
-              <image
-                class="item-content-img"
-                src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg"
-              ></image>
-              <view>
-                <uni-cellItem title="位置：" :value="item.build"></uni-cellItem>
-                <uni-cellItem title="时间：" :value="item.time"></uni-cellItem>
-              </view>
-            </view>
-          </view>
-        </u-list-item>
-        <u-loadmore
-          :status="laoding"
-          loadingIcon="semicircle"
-          height="88"
-          fontSize="32rpx"
-          @loadmore="loadmore"
-        />
-      </u-list>
+          </u-list-item>
+          <u-loadmore
+            :status="laoding"
+            loadingIcon="semicircle"
+            height="88rpx"
+            fontSize="32rpx"
+            @loadmore="loadmore"
+          />
+        </u-list>
+      </view>
     </view>
   </view>
 </template>
@@ -67,84 +54,62 @@ import "mui-player/dist/mui-player.min.css";
 export default {
   data() {
     return {
-      searchValue: "",
-      filterPanel: false,
+      columns: [
+        {
+          id: 2,
+          label: '牧场2',
+          children: [
+            {id: 21, label: '厂1', children: [{id:1, label: '栏1'}]},
+            {id: 22, label: '厂2'}
+          ]
+        }
+      ],
       listData: [
         {
           title: "濒死告警",
           status: "1",
           build: "一厂/2栏/3圈",
-          time: "2022-12-12 12:12"
+          time: "2022-12-12 12:12",
+          select: []
         },
         {
           title: "死亡告警",
-          status: "1",
+          status: "2",
           build: "一厂/2栏/3圈",
-          time: "2022-12-12 12:12"
-        },
-        {
-          title: "环境异常",
-          status: "1",
-          build: "一厂/2栏/3圈",
-          time: "2022-12-12 12:12"
+          time: "2022-12-12 12:12",
+          select: []
         },
         {
           title: "濒死告警",
           status: "1",
           build: "一厂/2栏/3圈",
-          time: "2022-12-12 12:12"
+          time: "2022-12-12 12:12",
+          select: []
         },
         {
           title: "死亡告警",
-          status: "1",
+          status: "2",
           build: "一厂/2栏/3圈",
-          time: "2022-12-12 12:12"
-        },
-        {
-          title: "环境异常",
-          status: "1",
-          build: "一厂/2栏/3圈",
-          time: "2022-12-12 12:12"
+          time: "2022-12-12 12:12",
+          select: []
         }
       ],
       pageNum: 1,
       laoding: "loadmore",
-      oneLevel: [
-        { label: "全部", id: "0" },
-        { label: "一厂", id: 1 },
-        { label: "二厂", id: 2 },
-        { label: "三厂", id: 3 },
-        { label: "四厂", id: 4 }
-      ],
-      twoLevel: [
-        { label: "全部", id: "0" },
-        { label: "一栏", id: 1 },
-        { label: "二栏", id: 2 },
-        { label: "三栏", id: 3 },
-        { label: "四栏", id: 4 }
-      ],
-      threeLevel: [
-        { label: "全部", id: "0" },
-        { label: "一圈", id: 1 },
-        { label: "二圈", id: 2 }
-      ],
-      status: "",
-      state: ""
-    };
+    }
   },
-
   computed: {
     windowHeight() {
       return uni.getSystemInfoSync().windowHeight;
-    },
-    listHeight() {
-      let safe = uni.getSystemInfoSync().safeAreaInsets;
-      return safe.top + safe.bottom;
     }
   },
   methods: {
+    treeCallback(value) {
+      this.fieldId = value.id[0]
+    },
     loadmore() {
       this.pageNum += 1;
+      console.log(this.laoding)
       if (this.laoding == "loadmore") {
         this.getList();
       }
@@ -193,124 +158,45 @@ export default {
 
 <style lang="scss" scoped>
 .list-container {
-}
-.search-view {
-  padding: 12rpx 24rpx;
-  background-color: #ffffff;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  .search-input {
-    width: 80%;
-    background: #f5f5f5;
-    color: #333333;
-  }
-  .filter-panel {
-    width: 100%;
-    position: absolute;
-    left: 0;
-    top: 96rpx;
-    background-color: rgba(0, 0, 0, 0.5);
-    z-index: 100;
-    .filter-panel-content {
-      background-color: #ffff;
-      padding: 0 30rpx 30rpx;
-      .filter-title {
-        color: #000000;
-        font-size: 30rpx;
-        font-weight: 500;
-        padding: 30rpx 0;
-      }
-      .state-list {
+  .content {
+    background: linear-gradient(to bottom, #D6E7FF 0%, #FFFFFF 600rpx);
+    padding: 0 24rpx 48rpx;
+    .video-section {
+      width: 100%;
+      height: 400rpx;
+      background: #333333;
+      margin-bottom: 24rpx;
+    }
+    .warin-section {
+      .list-item {
+        width: 100%;
+        height: 258rpx;
         display: flex;
-        flex-wrap: wrap;
         justify-content: flex-start;
-        .state-item {
-          width: 210rpx;
-          height: 72rpx;
-          border: 1rpx solid rgba(0, 0, 0, 0.25);
-          border-radius: 72rpx;
-          text-align: center;
-          line-height: 72rpx;
-          margin: 0 20rpx 20rpx 0;
-          font-size: 28rpx;
-          color: #000000;
-        }
-        .active {
-          background-color: rgba(222, 241, 255, 1);
-          border: 1rpx solid rgba(22, 119, 255, 1);
+        align-items: center;
+        margin-bottom: 24rpx;
+        padding: 24rpx;
+        background: #deebff;
+        border-radius: 16rpx;
+        .item-info {
+          height: 210rpx;
+          margin-left: 24rpx;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          align-items: flex-start;
+          position: relative;
+          .status-tag {
+            width: 108rpx;
+            margin: 12rpx 0;
+          }
+          .select-item {
+            position: absolute;
+            top: 0;
+            right: 0;
+          }
         }
       }
-    }
-    .btn-box {
-      display: flex;
-      padding: 24rpx 30rpx;
-      background-color: #fff;
-      box-shadow: 0rpx -10rpx 20rpx #eeeeee;
-    }
-  }
-}
-.list {
-  margin-top: 20rpx;
-}
-.list-item {
-  /* padding: 24rpx; */
-  /* margin: 24rpx; */
-  margin-bottom: 24rpx;
-  border-radius: 16rpx;
-  background-color: #fff;
-  .item-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-bottom: 16rpx;
-    .status {
-      .status-item {
-        width: 120rpx;
-        height: 44rpx;
-        text-align: center;
-        line-height: 44rpx;
-        border-radius: 4rpx;
-        color: rgba(255, 85, 51, 1);
-        background-color: rgba(255, 85, 51, 0.2);
-        font-size: 24rpx;
-      }
-    }
-  }
-  .item-content {
-    display: flex;
-    .title {
-      width: 50rpx;
-    }
-  }
-  .item-content-img {
-    height: 180rpx;
-    width: 300rpx;
-    margin-right: 20rpx;
-  }
-}
-.section {
-  margin: 24rpx;
-  padding: 24rpx;
-  background-color: #fff;
-  border-radius: 8rpx;
-  .section-title {
-    width: 160rpx;
-    color: #333333;
-    line-height: 44rpx;
-    font-size: 30rpx;
-    border-left: 6rpx solid #2681ff;
-    padding-left: 26rpx;
-    font-weight: bold;
-  }
-  .section-video {
-    margin-top: 20rpx;
-  }
-  .form-view {
-    padding: 20rpx 0rpx 0 10rpx;
-    .form-btn {
-      padding-top: 20rpx;
     }
   }
 }
