87c3a1a688fd73e10946984985c0f1e10170a788
何迪斐 <7424185+he_di_fei@user.noreply.gitee.com>
2024-05-02 12:04:31 +0800

    feat: 更新chat页面

--- src/pages/chat/index.vue
+++ src/pages/chat/index.vue
@@ -1,8 +1,16 @@
 <template>
   <view class="bg">
-    <view v-if="ptzt == 0"> </view>
-    <view v-if="ptzt == 1">
-      <scroll-view scroll-with-animation scroll-y="true" style="width: 100%">
+    <view class="content">
+      <uni-navtopbar title="畜牧助手" :back="true"></uni-navtopbar>
+
+      <scroll-view
+        class="msg-list"
+        :scroll-into-view="scrollIntoView"
+        scroll-with-animation
+        scroll-y="true"
+        style="width: 100%"
+        :enable-flex="true"
+      >
         <!-- 用来获取消息体高度 -->
         <view id="okk" scroll-with-animation>
           <!-- 消息 -->
@@ -54,38 +62,42 @@
             </view>
           </view>
           <!-- 防止消息底部被遮 -->
-          <view style="height: 130rpx"></view>
         </view>
+        <!-- 最后一个最新元素用来定位滚动 -->
+        <view id="last-msg-item" style="height: 1px"></view>
       </scroll-view>
 
       <!-- 底部导航栏 -->
-      <view
-        class="bottom-navigation"
-        style="position: fixed; bottom: 0px; width: 100%"
-      >
-        <view class="bottom-bar flex-row-around" style="font-size: 55rpx">
-          <!-- vue无法使用软键盘"发送" -->
-          <input
-            v-model="msg"
-            class="input-field"
-            type="text"
-            style="background-color: #f0f0f0; font-size: 25rpx"
-            @confirm="sendMsg"
-            confirm-type="search"
-            placeholder-class="placeholder-text"
-            placeholder="用一句简短的话描述您的问题"
-          />
-          <button
-            @click="sendMsg"
-            :disabled="msgLoad"
-            class="send-button bg-cyan round"
-          >
-            发送
-          </button>
+      <view class="foot-box">
+        <view class="foot-box-content">
+          <view class="textarea-box">
+            <textarea
+              v-model="msgContent"
+              :cursor-spacing="15"
+              class="textarea"
+              :auto-height="true"
+              placeholder="请输入要你的问题"
+              :maxlength="-1"
+              :adjust-position="false"
+              :disable-default-padding="false"
+              placeholder-class="input-placeholder"
+            ></textarea>
+          </view>
+          <view class="send-btn-box">
+            <button
+              @click="send"
+              :disabled="inputBoxDisabled || !msgContent"
+              class="send"
+              type="primary"
+            >
+              发送
+            </button>
+          </view>
         </view>
       </view>
     </view>
-    <uni-tabbar :tabCurrent="2"></uni-tabbar>
+
+    <!-- <uni-tabbar :tabCurrent="2"></uni-tabbar> -->
   </view>
 </template>
 
@@ -93,7 +105,7 @@
 export default {
   data() {
     return {
-      msgLoad: true,
+      msgLoad: false,
       anData: {},
       userId: "",
       animationData: {},
@@ -102,78 +114,170 @@ export default {
         {
           my: false,
           msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: true,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: false,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: true,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: false,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: true,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: false,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: true,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: false,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: true,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: false,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: true,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: false,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
-        },
-        {
-          my: true,
-          msg: "你好我是畜牧助手,请问有什么问题可以帮助您?"
         }
       ],
       msgContent: "",
       msg: "",
-      ptzt: 1
+      ptzt: 1,
+      keyboardHeight: 0,
+      // 使聊天窗口滚动到指定元素id的值
+      scrollIntoView: ""
     };
   },
   onLoad() {
-    uni.hideTabBar()
+    uni.hideTabBar();
   },
-  methods: {}
+  computed: {
+    footBoxPaddingBottom() {
+      return (this.keyboardHeight || 20) + "rpx";
+    }
+  },
+  methods: {
+    // 滚动窗口以显示最新的一条消息
+    showLastMsg() {
+      // 等待DOM更新
+      this.$nextTick(() => {
+        // 将scrollIntoView属性设置为"last-msg-item"，以便滚动窗口到最后一条消息
+        this.scrollIntoView = "last-msg-item";
+        console.log("读取scroll高度 ", this.scrollIntoView);
+        // 等待DOM更新，即：滚动完成
+        this.$nextTick(() => {
+          // 将scrollIntoView属性设置为空，以便下次设置滚动条位置可被监听
+          this.scrollIntoView = "";
+        });
+      });
+    },
+    // 清空消息列表
+    clearAllMsg(e) {
+      // 弹出确认清空聊天记录的提示框
+      uni.showModal({
+        title: "确认要清空聊天记录？",
+        content: "本操作不可撤销",
+        complete: e => {
+          // 如果用户确认清空聊天记录
+          if (e.confirm) {
+            // 将消息列表清空
+            this.msgList.splice(0, this.msgList.length);
+          }
+        }
+      });
+    },
+    send() {
+      const curUserMsgList = [
+        {
+          my: true,
+          msg: this.msgContent
+        }
+      ];
+      const curAnswerList = [
+        {
+          my: false,
+          msg: "测试回答"
+        }
+      ];
+      //清空输入框
+      this.msgContent = "";
+      this.msgList = this.msgList.concat(curUserMsgList);
+      this.msgLoad = true;
+      this.showLastMsg();
+
+      //模拟一秒后关闭loading
+      setTimeout(() => {
+        this.msgLoad = false;
+        this.msgList = this.msgList.concat(curAnswerList);
+        this.showLastMsg();
+      }, 1000);
+      console.log(this.msgContent);
+    }
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .bg {
-  padding: 20rpx;
+  /* padding: 20rpx; */
+  height: 100vh;
   background-color: rgb(240, 240, 240);
+  .content {
+    background: linear-gradient(to bottom, #d6e7ff 0%, #ffffff 600rpx);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    height: 100%;
+    .msg-list {
+      flex: 1;
+      padding: 24rpx 24rpx 0 24rpx;
+      overflow: hidden;
+    }
+  }
 }
 
+/* 底部 */
+.foot-box {
+  width: 750rpx;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0px;
+  background-color: #fff;
+  background: linear-gradient(to bottom, #d6e7ff 0%, #ffffff 600rpx);
+}
+
+.foot-box-content {
+  display: flex;
+  justify-content: space-around;
+}
+
+.textarea-box {
+  padding: 8px 10px;
+  background-color: #f9f9f9;
+  border-radius: 5px;
+}
+
+.textarea-box .textarea {
+  max-height: 120px;
+  font-size: 14px;
+  /* #ifndef APP-NVUE */
+  overflow: auto;
+  /* #endif */
+  width: 450rpx;
+  font-size: 14px;
+}
+
+/* #ifdef H5 */
+/*隐藏滚动条*/
+.textarea-box .textarea::-webkit-scrollbar {
+  width: 0;
+}
+/* #endif */
+
+.trash,
+.send {
+  width: 50px;
+  height: 30px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+}
+
+.send {
+  color: #fff;
+  border-radius: 4px;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  font-size: 14px;
+  margin-right: 20rpx;
+}
+
+/* #ifndef APP-NVUE */
+.send::after {
+  display: none;
+}
+
+/* #endif */
+
+/* 其他 */
 .chat-robot-img {
   display: flex;
   align-items: center;
@@ -221,16 +325,6 @@ export default {
   background-color: #f9f9f9;
 }
 
-.loading-message {
-  display: flex;
-}
-
-.bottom-navigation {
-  position: fixed;
-  bottom: 0px;
-  width: 100%;
-}
-
 .bottom-bar {
   display: flex;
 }
@@ -255,6 +349,12 @@ export default {
   height: 1em;
   background-color: #000000;
   animation: blink 1s infinite;
+}
+
+.bottom-navigation {
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
 }
 
 @keyframes blink {
