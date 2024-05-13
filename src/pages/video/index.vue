5e659bbf53921c1d7ba1926d0f67cf0de74d5f78
何迪斐 <7424185+he_di_fei@user.noreply.gitee.com>
2024-05-14 01:11:27 +0800

    feat: 修改实况视频直播情况

---
+++ src/pages/video/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <web-view
+    src="https://m.zzxmt.cn/html/player.html"
+    :style="{ width: '100%', height: '100%' }"
+  ></web-view>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: false
+    }
+  }
+};
+</script>
